d27a25b8363ce8175c15b4227a863c67c12f8b04
qqn2 <samy_b4l@hotmail.fr>
2020-09-30 16:30:50 +0200

    generator

--- RTL/generator.sv
+++ RTL/generator.sv
@@ -1,13 +1,27 @@
 class generator;
 
-rand transaction trans;
+	rand transaction trans;
+	//declaring mailbox
+	mailbox gen2driv;
+	//repeat count, to specify number of items to generate
+  	int  repeat_count;  
+	//event
+	event ended;
+	//constructor
+	function new(mailbox gen2driv, event ended);
+		//getting the mailbox handle from env
+		this.gen2driv = gen2driv;
+		this.ended  = ended;
+	endfunction
 
-task main();
-	trans = new();
-	
-endtask : main
-
-
+	//main task, generates(create and randomizes) the packets and puts into mailbox
+	task main();
+		repeat(repeat_count) begin
+		trans = new();
+		if( !trans.randomize() ) $fatal("Gen:: trans randomization failed");
+		gen2driv.put(trans);
+		end
+	endtask
 
 
 
