35d0c1ee7ec895a8a51e284979bdc4826329767b
qqn2 <samy_b4l@hotmail.fr>
2020-10-02 15:08:37 +0200

    tranquility

--- SIM/waves.tcl
+++ SIM/waves.tcl
@@ -1,11 +1,11 @@
 add wave -r sim:/tb/DUT/clk
 add wave -r sim:/tb/DUT/rst_n
-add wave -r sim:/tb/DUT/push_data_i
-add wave -r sim:/tb/DUT/push_valid_i
-add wave -r sim:/tb/DUT/push_grant_o
-add wave -r sim:/tb/DUT/pop_data_o
-add wave -r sim:/tb/DUT/pop_valid_o
-add wave -r sim:/tb/DUT/pop_grant_i
+add wave -r sim:/tb/DUT/data_i
+add wave -r sim:/tb/DUT/valid_i
+add wave -r sim:/tb/DUT/grant_o
+add wave -r sim:/tb/DUT/data_o
+add wave -r sim:/tb/DUT/valid_o
+add wave -r sim:/tb/DUT/grant_i
 add wave -r sim:/tb/i
 add wave -r sim:/tb/j
 
